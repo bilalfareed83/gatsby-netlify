63b9cb2a04cddbeddd3205a5270bc90e8622e5bb
bilalfareed83 <bilal_fareed@hotmail.com>
2020-12-17 16:35:20 +0500

    add netlify function

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,24 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <h2>My first netlify deployment updated version</h2>
-  </Layout>
-)
+const IndexPage = () => {
+  const [val, setVal] = useState({})
+
+  useEffect(() => {
+    fetch("/.netlify/functions/hello?name=server less func")
+      .then(res => res.json())
+      .then(obj => setVal(obj))
+  }, [])
+  console.log(val.message)
+  return (
+    <Layout>
+      <div>{val.message}</div>
+    </Layout>
+  )
+}
 
 export default IndexPage
